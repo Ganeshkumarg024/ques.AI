d66eb876aba3c6bd28cd51770569689e28701267
Ganeshkumarg024 <ganeshkumarg024@gmail.com>
2024-09-04 01:05:37 +0530

    initial Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+
+import CreateProject from './components/Project/CreateProject.js';
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Home/> */}
+      <CreateProject/>
+    </div>
+  );
+}
+
+export default App;
